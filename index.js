cfd25c1d338e7bba24cec474475e3c7500cc35ce
MK404xL <shinydark.sky@gmail.com>
2020-07-31 20:03:03 +0700

    button iphone on html and css

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
 const port = 3000
-const mongoDB = 'mongodb://127.0.0.1:3030/todo-list'
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.set('views', 'Views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
 app.get("/",(req,res)=>{
     res.send('test')
 })
-
+app.get("/",(req,res)=>{
+    res.send('test')
+})
+app.get("/button",(req,res)=>{
+    res.render('button')
+})
 app.listen(port,()=>console.log(`()=>port ${port}`))
