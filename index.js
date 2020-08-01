567567ad8b3a36343d70f4a94ec01e753579c6e9
MK404xL <shinydark.sky@gmail.com>
2020-08-01 20:25:54 +0700

    create schema model

--- index.js
+++ index.js
@@ -6,23 +6,19 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.listen(port,()=>console.log(`()=>port ${port}`))
 // Express
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://thanhloc:CECwH6ipKW1jj8HC@cluster0.cbei7.gcp.mongodb.net/tutorial?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// Body parser 
+let mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://thanhloc:EVL6sfbDcNsuJ8fY@cluster0.cbei7.gcp.mongodb.net/tutorial?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
     if(!err){
         console.log('connect to mongodb successed')
     }else{
         console.log('connect to mongodb failed')
     }
 });
-// Mongoose
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-// Body parser
-
-
-
-
+// Mongoose pass EVL6sfbDcNsuJ8fY
 app.get("/",(req,res)=>{
     res.send('test')
 })
---
+++ models/cap2.js
@@ -0,0 +1,10 @@
+let mongoose = require('mongoose');
+
+
+const cap2Schema = new mongoose.Schema({
+    name:String,
+    kids:[{type:mongoose.Schema.Types.ObjectId}]
+})
+
+
+module.exports = mongoose.model('cap2',cap2Schema)
