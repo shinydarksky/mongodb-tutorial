5f23b51b0f20ed2198b1a44be4a458f0371fa9d2
MK404xL <shinydark.sky@gmail.com>
2020-08-03 23:24:32 +0700

    Layout create

--- controller/controller.Create.js
+++ controller/controller.Create.js
@@ -4,5 +4,21 @@ var cap2s = require('../Models/cap2')
 
 
 module.exports = (req,res)=>{
-    res.render('create')
+    var cap1 = cap1s.aggregate(
+        [{
+            $lookup:
+            {
+                from:"cap2",
+                localField:"kids",
+                foreignField:"_id",
+                as:"con"
+            }
+        }]
+        ,(err,data)=>{
+            if(!err){
+                res.render('create',{data:data})
+            }else{
+                res.json("fail "+"errMsg: "+err)
+            }
+        })   
 }
